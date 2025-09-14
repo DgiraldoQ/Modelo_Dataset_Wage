9111d46c9bbc2dda848b5bee308da24d44f86f10
GiraldoAnalytics <diego.54.461@gmail.com>
2025-09-14 16:50:12 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -6,8 +6,14 @@ import os
 st.set_page_config(page_title="Predicción de Salarios", layout="wide")
 
 # Obtener la URL de la API
-API_URL = os.environ.get("API_URL", "https://modelo-dataset-wage.onrender.com")
-GOOGLE_API_KEY = os.environ.get("GOOGLE_API")
+def get_api_url():
+    try:
+        return st.secrets["API_URL"]
+    except Exception:
+        return os.environ.get("API_URL", "http://127.0.0.1:8000")
+
+API_URL = get_api_url().rstrip("/")
+
 
 
 st.title("💼 Dashboard - Predicción de Salarios")
