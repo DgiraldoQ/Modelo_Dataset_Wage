0da1a2fb7e238f1381938a56f10e19d63491c5cc
GiraldoAnalytics <diego.54.461@gmail.com>
2025-09-16 22:39:42 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -218,7 +218,7 @@ El grupo con educación incompleta universitaria (“Some College”) presenta e
 superando incluso a graduados universitarios y con posgrado. Esto sugiere que factores adicionales, como la experiencia laboral o el sector de ocupación, influyen de forma significativa en el nivel salarial. En contraste, 
 los individuos con menor nivel educativo (“< HS Grad”) obtienen los salarios más bajos (84.10), evidenciando la brecha económica vinculada a la educación.
 
-![Resumen estadístico](img/educacion y salario.png)
+![Resumen estadístico](img/educacion%20y%20salario.png)
 
 
 
